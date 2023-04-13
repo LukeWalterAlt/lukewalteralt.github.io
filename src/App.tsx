ec1dbc34324ee3161bdb30e4f555c78ebdcd98d9
Luke Walter <59621677+LukeWalter@users.noreply.github.com>
2023-04-13 14:55:55 -0400

    Added gbagt files

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from 'react-router-dom';
+import Navbar from "./components/global/Navbar"
+import About from "./pages/About"
+import Resources from "./pages/Resources"
+import Documentation from "./pages/Documentation"
+import HallOfFame from "./pages/HallOfFame"
+import ErrorPage from "./pages/ErrorPage";
+
+export default function App() {
+
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode as any}>
+      <ThemeProvider theme={theme as any}>
+        <CssBaseline>
+          <div className="app">
+            <main className="content">
+              <BrowserRouter>
+                <Navbar />
+                <Routes>
+                  <Route path="/" element={ <About /> } />
+                  <Route path="/resources" element={ <Resources /> } />
+                  <Route path="/docs" element={ <Documentation /> } />
+                  <Route path="/hall-of-fame" element={ <HallOfFame /> } />
+                  <Route path='*' element={ <ErrorPage /> }/>
+                </Routes>
+              </BrowserRouter>
+            </main>
+          </div>
+        </CssBaseline>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
